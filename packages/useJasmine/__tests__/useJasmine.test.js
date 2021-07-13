fd66b96e5d003e9a16eba8b33a7f7cba1b949c25
feigebaobei <18515195415@163.com>
2021-07-13 10:43:32 +0800

    feat(useJasmine): 使用js api执行测试文件

--- packages/useJasmine/__tests__/useJasmine.test.js
+++ packages/useJasmine/__tests__/useJasmine.test.js
@@ -6,7 +6,7 @@ describe('useJasmine', () => {
     it('needs tests', () => {
         expect(usejasmine.add(1, 2)).toEqual(3)
     });
-    it('needs tests', () => {
+    it('needs tests 2', () => {
         expect(usejasmine.add(1, 2)).toEqual(4)
     });
 });
